93e3673eeb1060a1af4663d53e0a981b598556ba
Zhminko Roman <92923411+ZhminkoRoman@users.noreply.github.com>
2023-01-24 18:40:20 +0300

    fix: fix global css import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import '@/pages/globals.css';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import { ReactElement, ReactNode } from 'react';
+import './globals.css';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
+  getLayout?: (_page: ReactElement) => ReactNode;
 };
 
 type AppPropsWithLayout = AppProps & {
