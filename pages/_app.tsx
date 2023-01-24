2dde9627cc635db96caee7ae70ee8109e3831d55
Zhminko Roman <92923411+ZhminkoRoman@users.noreply.github.com>
2023-01-24 18:49:58 +0300

    fix: fix tailwind css file issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import { ReactElement, ReactNode } from 'react';
-import './globals.css';
+import '../styles/globals.css';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (_page: ReactElement) => ReactNode;
